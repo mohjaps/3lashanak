25f86105dc65ab78d39873c85a1f1269d48728aa
Mo Jaber <moh.ja.ps@gmail.com>
2022-05-18 14:49:11 +0300

    Classes

---
+++ 3lashanak/Models/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace _3lashanak.Models
+{
+    public class Messages
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime SendDate { get; set; } = DateTime.Now;
+    }
+}
---
+++ 3lashanak/Models/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3lashanak.Models
+{
+    public enum TypeSettings { Button, Text}
+    public class Settings
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Icon { get; set; }
+        public TypeSettings Type { get; set; }
+    }
+}
